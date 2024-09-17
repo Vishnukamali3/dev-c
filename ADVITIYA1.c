5b688e9c6add46659f6874bc2283bb8dacd17cc1
Vishnukamali3 <166210006+Vishnukamali3@users.noreply.github.com>
2024-09-17 21:45:17 +0530

    Add files via upload

---
+++ ADVITIYA1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    if(n==16 || n==17 || n==18)
+    {
+        printf("ADVITIYA");
+    }
+    else
+    {
+        printf("WAITING FOR ADVITIYA");
+    }
+}
