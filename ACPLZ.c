8bce0fa8d6373802ead648f55bce66cd1f078011
Vishnukamali3 <166210006+Vishnukamali3@users.noreply.github.com>
2024-09-17 21:45:53 +0530

    Add files via upload

---
+++ ACPLZ.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+	// your code goes here
+    int n;
+    scanf("%d",&n);
+    if (n<=30)
+    {
+        printf("NO");
+        
+    }
+    else
+    {
+        printf("YES");
+    }
+}
+
